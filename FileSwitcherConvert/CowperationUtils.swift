6ab09c38810f0d7034ae6031fb3c8a9f41b80429
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-26 15:14:46 -0500

    zipfoundation

---
+++ FileSwitcherConvert/CowperationUtils.swift
@@ -0,0 +1,17 @@
+//
+//  CowperationUtils.swift
+//  FileSwitcherConvert
+//
+//  Created by Hariz Shirazi on 2023-02-26.
+//
+
+import Foundation
+
+class Cowperation {
+    func open() {
+        // open file
+    }
+    func write() {
+        // write to disk
+    }
+}
---
+++ FileSwitcherConvert/FspUtils.swift
@@ -0,0 +1,17 @@
+//
+//  FspUtils.swift
+//  FileSwitcherConvert
+//
+//  Created by Hariz Shirazi on 2023-02-26.
+//
+
+import Foundation
+
+class Fsp {
+    func open() {
+        // open file
+    }
+    func write() {
+        // write to disk
+    }
+}
