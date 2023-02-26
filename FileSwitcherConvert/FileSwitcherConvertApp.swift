c95cb27d5083fcf52046c42d52727417b5854447
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-26 13:53:22 -0500

    Initial Commit

---
+++ FileSwitcherConvert/FileSwitcherConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSwitcherConvertApp.swift
+//  FileSwitcherConvert
+//
+//  Created by Hariz Shirazi on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct FileSwitcherConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
