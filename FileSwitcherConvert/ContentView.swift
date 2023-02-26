0b53400eb632418ddfcaa2b15b45f6659878c455
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-26 14:53:20 -0500

    give it that signature bomberfish look

--- FileSwitcherConvert/ContentView.swift
+++ FileSwitcherConvert/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Label("eta son ;)", systemImage: "clock")
+            }
+            .navigationTitle("FileSwitcherConvert")
         }
-        .padding()
     }
 }
 
